7b1776ffa964edd1e815511b91df1992814b6a88
ParshinSA <Parshin.Sergey222@mail.ru>
2023-12-11 14:18:27 +0300

    delete rxJava2 some files

--- app/src/main/java/com/example/unsplash/_common/extensions/Dispose.kt
+++
@@ -1,8 +0,0 @@
-package com.example.unsplash._common.extensions
-
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-fun Disposable.autoAdd(rxContainer: CompositeDisposable) {
-    rxContainer.add(this)
-}
